84829ef0733b93ce86b67869a2dc2d5327ad8546
Romuch1not1first <rtkacenko9@gmail.com>
2024-10-17 13:49:34 +0300

    Query parameter processing

--- app/main.py
+++ app/main.py
@@ -1,15 +1,10 @@
-from typing import Annotated # про это будет чуть позднее в курсе
-
-from fastapi import FastAPI, File, UploadFile
+from fastapi import FastAPI
 
 app = FastAPI()
 
-
-@app.post("/files/")
-async def create_file(file: Annotated[bytes, File()]):
-    return {"file_size": len(file)}
+fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
 
 
-@app.post("/uploadfile/")
-async def create_upload_file(file: UploadFile):
-    return {"filename": file.filename}
+@app.get("/items/")
+async def read_item(skip: int = 0, limit: int = 10):
+    return fake_items_db[skip : skip + limit]
