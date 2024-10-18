4efb95c231ec000fa52e0968e6f2d8c4aa97fd81
Romuch1not1first <rtkacenko9@gmail.com>
2024-10-18 12:37:46 +0300

    headers

--- app/main.py
+++ app/main.py
@@ -1,9 +1,19 @@
-from typing import Optional
-from fastapi import FastAPI, Header
+from fastapi import FastAPI, Request, HTTPException
 
 app = FastAPI()
 
+@app.get("/headers")
+def get_headers(request: Request):
+    # Извлечение заголовков
+    user_agent = request.headers.get("user-agent")
+    accept_language = request.headers.get("accept-language")
 
-@app.get("/items/")
-async def read_items(user_agent: Optional[str] = Header(None)):
-    return {"User-Agent": user_agent}
+    # Проверка на наличие заголовков
+    if not user_agent or not accept_language:
+        raise HTTPException(status_code=400, detail="Missing required headers: 'User-Agent' or 'Accept-Language'")
+    
+    # Возврат значений заголовков в ответе
+    return {
+        "User-Agent": user_agent,
+        "Accept-Language": accept_language
+    }
